28f45a87587c9862f130716b7cce548f25313453
akshay <akshay.koul@aginsurance.be>
2021-05-08 16:09:23 +0200

    fixed Box story

--- src/components/individual/Box.stories.js
+++ src/components/individual/Box.stories.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import Box  from './Box.stories';
-
+import Box  from './Box';
+import Line from './Line'
 //👇 This default export determines where your story goes in the story list
 export default {
   title: 'Box',
   component: Box,
-};
+  subcomponents:{ Line },
+}; 
 
 //👇 We create a “template” of how args map to rendering
-const Template = (args) => <Box {...args} />;
+const Template = (args) => <Box {...args}><Line/></Box>;
 
 export const FirstStory = Template.bind({});
 
-FirstStory.args = {
-  children:`test hello`
-};
+//  FirstStory.children= {
+//    <p>hi</p>]
+//  };
